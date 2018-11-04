9fb58b96611fc7904b6cdcfcd60fbdf5926f8228
GichevaMargarita <margoshka_love20@mail.ru>
2018-11-04 22:34:42 +0100

    Updates

---
+++ precache-manifest.b60d02e0c4506041bbbec95669eb29ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f378f9cf597d1002f1a6",
+    "url": "/color-mix/static/css/main.a8a9bf3f.chunk.css"
+  },
+  {
+    "revision": "f378f9cf597d1002f1a6",
+    "url": "/color-mix/static/js/main.f378f9cf.chunk.js"
+  },
+  {
+    "revision": "37f90d57916b981a0157",
+    "url": "/color-mix/static/js/1.37f90d57.chunk.js"
+  },
+  {
+    "revision": "552906c52e98b9b424fa",
+    "url": "/color-mix/static/js/runtime~main.552906c5.js"
+  },
+  {
+    "revision": "fc3a2d2ac251c6ba7a4ff815a880e6f2",
+    "url": "/color-mix/index.html"
+  }
+];
